35b5319743685c659332ae6a85ef74bb1897bad3
zks0002 <zks0002@gmail.com>
2013-02-13 19:45:44 -0700

    added JK and D flip flop files and started drawing the D

--- scripts/src/dff.js
+++ scripts/src/dff.js
@@ -7,31 +7,20 @@
  *  Zack Sheffield
  ******************************************************************************/
 
-function XOR(numInputs) {
-    this.type = digsim.XOR;
-    this.name = 'XOR';
+function DFF(numInputs) {
+    this.type = digsim.DFF;
+    this.name = 'DFF';
     
     this.next = [];
     this.prev = [];
     this.prevConnect = [];
     this.connections = [];
     this.juncts = [];
-    this.numInputs = numInputs || 2;
-    var size = (2 * (Math.floor(this.numInputs / 2))) + 1;
-    this.dimension = {'row': size, 'col': (size + 1)};
-
-    this.outPt = 2;
+    this.numInputs = 2;
+    this.dimension = {'row': 3, 'col': 2};
 };
-XOR.prototype = new Drawable();
 
-/*****************************************************************************
- * CHANGE SIZE
- *  Changes the size of the gate based on numInputs
- ****************************************************************************/
-XOR.prototype.changeSize = function() {
-    var size = (2 * (Math.floor(this.numInputs / 2))) + 1;
-    this.dimension = {'row': size, 'col': (size + 1)};
-}
+DFF.prototype = new Drawable();
 
 /*****************************************************************************
  * DRAW
@@ -39,7 +28,7 @@ XOR.prototype.changeSize = function() {
  *  handle any number of inputs. Props to Steven Lambert for figuring out how
  *  to draw a half circle with the bezierCurveTo method. 
  ****************************************************************************/
-XOR.prototype.draw = function(context, lineColor) {
+DFF.prototype.draw = function(context, lineColor) {
     
     context.save();
     context.translate(this.col * digsim.GRID_SIZE, this.row * digsim.GRID_SIZE);
@@ -48,6 +37,7 @@ XOR.prototype.draw = function(context, lineColor) {
     context.strokeStyle = lineColor || 'black';
     context.lineWidth = 2;
 
+    // Rotatation
     var offsetH = 0, offsetV = 0;
     if (this.rotation == 90) {
         offsetV = -0.5;
@@ -62,55 +52,14 @@ XOR.prototype.draw = function(context, lineColor) {
     context.translate(center.col, center.row);
     context.rotate(this.rotation * Math.PI / 180);
     context.translate(-center.col, -center.row);
-    
-    this.drawWires(context, lineColor);
-    
+        
     // Draw gate
-    var factor = Math.floor(this.numInputs / 2); 
-    var gsf = digsim.GRID_SIZE * factor;
-    
     context.moveTo(0, 0);
-    context.lineTo(gsf,  0);            
+    context.lineTo(2 * digsim.GRID_SIZE,  0);
+    context.lineTo(2 * digsim.GRID_SIZE,  3 * digsim.GRID_SIZE);
+    context.lineTo(0,  3 * digsim.GRID_SIZE);
+    context.closePath();
     
-    // VECTOR CALCULUS... good luck. :)
-    
-    var t = 0.28;               // SET THIS TO CHANGE CURVATURE
-    var baseCurveature = 1.15;  // SET THIS TO CHANGE BASE CURVATURE
-    var height = 2 * factor + 1;
-    var x0 = gsf;
-    var y0 = 0;
-    var y1 = height * digsim.GRID_SIZE / 2;
-    var x1 = y1 * 2 + digsim.GRID_SIZE;
-    var xc = (x0 + x1) / 2;
-    var yc = (y0 + y1) / 2;
-    var x = (y1 - y0) * t + xc;
-    var y = (x0 - x1) * t + yc;
-    
-    context.quadraticCurveTo(x, y, x1, y1);
-    
-    x0 = x1;
-    y0 = y1;
-    x1 = gsf;
-    y1 = height * digsim.GRID_SIZE;
-    xc = (x0 + x1) / 2;
-    yc = (y0 + y1) / 2;
-    x = (y1 - y0) * t + xc;
-    y = (x0 - x1) * t + yc;
-    
-    context.quadraticCurveTo(x, y, x1, y1);
-    
-    context.lineTo(0, y1);
-    
-    context.lineWidth = 1;
-    context.quadraticCurveTo(digsim.GRID_SIZE * baseCurveature, y1 / 2, 
-                             0, 0);
-    context.stroke();
-    context.fill();
-
-    // base quadratic curve
-    context.beginPath();
-    context.moveTo(digsim.GRID_SIZE / -4, y1);
-    context.quadraticCurveTo(digsim.GRID_SIZE * baseCurveature - digsim.GRID_SIZE / 4, y1 / 2, digsim.GRID_SIZE / -4, 0);
     context.stroke();
     context.restore();
     
@@ -132,7 +81,7 @@ XOR.prototype.draw = function(context, lineColor) {
  * COMPUTE LOGIC
  *  ORs all the input wires together to set the current state of the gate. 
  ******************************************************************************/
-XOR.prototype.computeLogic = function() {  
+DFF.prototype.computeLogic = function() {  
 
     var cnt = 0;
     for (var i = 0; i < this.numInputs; ++i) {
